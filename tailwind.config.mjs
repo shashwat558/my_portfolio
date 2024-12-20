f216650e5269f2e0049e4080827d1dfceaf46aa4
shashwat558 <jainshashwat528@gmail.com>
2024-12-20 22:05:33 +0530

    .

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,14 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+        oleo: ['"Oleo Script"', 'cursive'],
+        outfit: ['Outfit', 'sans-serif'],
+        parkinsans: ['Parkinsans', 'sans-serif'],
+        teko: ['Teko', 'sans-serif'],
+      },
+		},
 	},
 	plugins: [],
 }
